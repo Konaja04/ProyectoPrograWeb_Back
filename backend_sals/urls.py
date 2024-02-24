84ac107f8c6cd19452f939c2f67edaef6e6e6833
HidekiS <20212604@aloe.ulima.edu.pe>
2024-02-23 22:18:58 -0500

    endpoint login
    response y login que trae codigo password nombre img

--- backend_sals/urls.py
+++ backend_sals/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import *
 urlpatterns = [
+    path("login-json", loginPostJsonEndpoint),
     path('ver-peliculas',verPeliculas),
     path('ver-salas',verSalas),
 ]
